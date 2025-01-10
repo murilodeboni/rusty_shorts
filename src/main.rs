a27e6ad2a6cd82e1374aab57003e0812be140fa3
Murilo Deboni <murilo.deboni@gmail.com>
2025-01-10 16:21:13 +0100

    create static file with tailwind

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ mod api;
 mod models;
 mod schema;
 
+use rocket::fs::{FileServer, relative};
 use rocket_sync_db_pools::{database, diesel};
-use diesel::prelude::*;
 use crate::api::shorten_url::shorten;
 use crate::api::delete_slug::delete_slug;
 use crate::api::redirect_to_original::redirect_to_original;
@@ -19,24 +19,9 @@ pub struct RustyShortsDB(diesel::PgConnection);
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .attach(RustyShortsDB::fairing()) // Attach the database fairing
-        .mount("/", routes![index]) // Mount routes
+        .attach(RustyShortsDB::fairing())
+        .mount("/", FileServer::from(relative!("static")))
         .mount("/", routes![redirect_to_original])
         .mount("/api", routes![shorten])
         .mount("/api", routes![delete_slug])
-}
-
-#[get("/")]
-async fn index(db: RustyShortsDB) -> String {
-    let result = db
-        .run(|conn| {
-            diesel::dsl::sql::<diesel::sql_types::BigInt>("SELECT COUNT(*) FROM urls")
-                .get_result::<i64>(conn)
-        })
-        .await;
-
-    match result {
-        Ok(count) => format!("Number of URLs in DB: {}", count),
-        Err(e) => format!("Error querying DB: {}", e),
-    }
 }
